f5eb3bbefbdc15e097d7425df2786d0862076c27
dkreeft <37153972+dkreeft@users.noreply.github.com>
2022-08-08 09:23:14 +0200

    add pre-commit and run

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 base_requirements = [
-    'Click>=7.0.0',
+    "Click>=7.0.0",
 ]
 
 dev_requirements = [
-    'pytest',
-    'pytest-flake8',
-    'pytest-cov',
+    "pytest",
+    "pytest-flake8",
+    "pytest-cov",
 ]
 
 build_requirements = [
-    'setuptools>=38.6.0',
-    'twine>=1.11.0',
-    'wheel>=0.31.0',
+    "setuptools>=38.6.0",
+    "twine>=1.11.0",
+    "wheel>=0.31.0",
 ]
 
 with open("README.md", "r") as fh:
@@ -27,23 +27,19 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dkreeft/d3tree",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=base_requirements,
     extras_require={
-        'dev': dev_requirements,
-        'build': [dev_requirements, build_requirements],
+        "dev": dev_requirements,
+        "build": [dev_requirements, build_requirements],
     },
-    entry_points={
-        'console_scripts': [
-            'd3tree = d3tree.cli:main'
-        ]
-    }
+    entry_points={"console_scripts": ["d3tree = d3tree.cli:main"]},
 )
--- src/d3tree/cli.py
+++ src/d3tree/cli.py
@@ -1,15 +1,16 @@
-import click
 from os import getcwd
+
+import click
 
 from d3tree import utils
 
 
 @click.command()
-@click.argument('path', default=getcwd())
+@click.argument("path", default=getcwd())
 def main(path: str):
     filepath_iter = utils.generator_from_path(path=path)
     utils.print_iterator(iter_=filepath_iter)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
