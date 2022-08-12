1b53a3b1d03b4119eb82cdca788776aeba5b6392
dkreeft <37153972+dkreeft@users.noreply.github.com>
2022-08-12 13:34:01 +0200

    add tests for templates

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -62,12 +62,32 @@ def test_main_output(runner, create_file_system, args, fn):
     filepath = create_file_system[0] / fn
     result = runner.invoke(cli=d3.main, args=[f"{args}{filepath}"])
     assert result.exit_code == 0
-    assert os.path.isfile(create_file_system[0] / fn)
+    assert os.path.isfile(filepath)
 
 
-@pytest.mark.parametrize("args, fn", [("-o ", ""), ("--output=", "")])
+@pytest.mark.parametrize("args, fn", [("-o", ""), ("--output=", "")])
 def test_main_output_no_filename(runner, create_file_system, args, fn):
     filepath = create_file_system[0] / fn
     result = runner.invoke(cli=d3.main, args=[f"{args}{filepath}"])
     assert result.exit_code == 1
-    assert not os.path.isfile(create_file_system[0] / fn)
+    assert not os.path.isfile(filepath)
+
+
+@pytest.mark.parametrize("template", [("tree"), ("circles"), ("flame"), ("treemap")])
+@pytest.mark.parametrize("args", [("-t"), ("--template=")])
+def test_main_template(runner, args, template):
+    result = runner.invoke(cli=d3.main, args=[f"{args}{template}"])
+    assert result.exit_code == 0
+    assert result.output
+
+
+@pytest.mark.parametrize("template", [("tree"), ("circles"), ("flame"), ("treemap")])
+@pytest.mark.parametrize("t_args", [("-t"), ("--template=")])
+@pytest.mark.parametrize("o_args", [("-o"), ("--output=")])
+def test_main_template_output(runner, create_file_system, t_args, o_args, template):
+    filepath = create_file_system[0] / template
+    result = runner.invoke(cli=d3.main, args=[f"{t_args}{template}", f"{o_args}{filepath}"])
+    assert result.exit_code == 0
+    assert not result.output
+    assert os.path.isfile(filepath)
+    os.remove(filepath)
